bbcef4b7d0a7a7dbfafc61dbcede722f5f08f85d
anass <anas.shaikh7827@gmail.com>
2025-01-31 11:19:02 +0530

    Implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -1,20 +1,19 @@
-from tkinter import Listbox
-
 import functions
 import FreeSimpleGUI as sg
-
-from bonus.bonus16 import window
-from cli import new_todo
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-list_box = sg.Listbox(value=functions.get_todos(), key='todos',
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 while True:
      event, values = window.read()
@@ -37,7 +36,16 @@ while True:
              todos[index] = new_todo
              functions.write_todos(todos)
              window['todo'].update(values=todos)
+         case "Complete":
+             todo_to_complete = values["todos"][0]
+             todos = functions.get_todos()
+             todos.remove(todo_to_complete)
+             functions.write_todos(todos)
+             window['todos'].update(values=todos)
+             window['todo'].update(value='')
 
+         case "Exit":
+             break
          case 'todos':
              window['todo'].update(value=values['todo'][0])
          case sg.WIN_CLOSED:
